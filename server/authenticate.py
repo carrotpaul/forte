89492c12f21d0cc1890ac89650c889f9744e6e12
carrotpaul <developer.rabbitt@gmail.com>
2017-07-04 21:44:56 -0400

    Basic route with basic authentication

---
+++ server/authenticate.py
@@ -0,0 +1,14 @@
+import os, hashlib
+from binascii import unhexlify
+
+def salt_and_pepper(byte_password):
+    salt = b'XHy5tpl2VHvn83A4_OUOP$x/6VcDBduPZ3wh/QF/b9Fk+fCoXXj12FCdhjc7J+Zry'
+    salted = hashlib.pbkdf2_hmac('sha256', byte_password, salt, 700000)
+
+    pepper = bytearray(os.environ.get('AUTH_SECRET'))
+    return hashlib.pbkdf2_hmac('sha256', salted, pepper, 500000)
+
+def authenticate(password):
+    hashed_password = salt_and_pepper(bytearray(password))
+    encrypted_password = unhexlify(os.environ.get('AUTH_PASSWORD'))
+    return (hashed_password == encrypted_password)
---
+++ server/routes.py
@@ -0,0 +1,10 @@
+from authenticate import authenticate
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/download', methods=['POST'])
+def download():
+    password = request.get_json()['token']
+    if authenticate(password):
+        return 'Authentication successful'
+    abort(401)
