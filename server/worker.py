b59174be6cf8a1d7a8993be716ae2723146b0915
carrotpaul <developer.rabbitt@gmail.com>
2017-08-07 12:09:05 -0400

    Removing unnecessary code and renaming lib to task

--- server/worker.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-from lib.download import execute_dl
-import os
-
-_worker = Celery(__name__, broker=os.environ.get('CELERY_BROKER_URL'))
-
-@_worker.task
-def download_from_source(source):
-    execute_dl(source)
