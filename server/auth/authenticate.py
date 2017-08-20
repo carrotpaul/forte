dce9761498c6c11e87939a65988aeed057504691
carrotpaul <developer.rabbitt@gmail.com>
2017-08-20 15:12:34 -0400

    Changing auth_cert to api_certificate.cred dictionary

--- server/auth/authenticate.py
+++ server/auth/authenticate.py
@@ -1,17 +1,14 @@
+from __future__ import print_function
 from binascii import unhexlify
-import os, hashlib
+import os, ast, hashlib
 
 class AuthenticationException(Exception):
     pass
 
 def get_auth_cert():
-    cert_file = open('/run/secrets/auth_cert', 'r')
-    cert_content = cert_file.readlines()
-
-    encrypted_password = cert_content[0].strip()
-    pepper = reduce(lambda x, y: x + y, cert_content[1:]).encode('string_escape')
-
-    return encrypted_password, pepper
+    with open('/run/secrets/api_auth_certificate', 'r') as cert:
+        auth_params_dict = ast.literal_eval(cert.read().replace('\n ', ''))
+        return auth_params_dict['encrypted_password'], auth_params_dict['pepper']
 
 def salt_and_pepper(byte_password, pepper):
     salt = b'XHy5tpl2VHvn83A4_OUOP$x/6VcDBduPZ3wh/QF/b9Fk+fCoXXj12FCdhjc7J+Zry'
@@ -19,13 +16,21 @@ def salt_and_pepper(byte_password, pepper):
     return hashlib.pbkdf2_hmac('sha256', salted, pepper, 500000)
 
 def authenticate(password):
-    encrypted_password, pepper = get_auth_cert()
-    encrypted_password = unhexlify(encrypted_password)
-    hashed_password = salt_and_pepper(bytearray(password, 'utf-8'),
-        bytearray(pepper.decode('string-escape')))
+    print ("Authenticating event....", end='')
 
-    auth_check = (hashed_password == encrypted_password)
-    print ("Authenticating event....%s" % ("Success" if auth_check else "Failure"))
+    try:
+        encrypted_password, pepper = get_auth_cert()
+        encrypted_password = unhexlify(encrypted_password)
+        hashed_password = salt_and_pepper(bytearray(password, 'utf-8'),
+            bytearray(pepper.decode('string-escape')))
 
-    if not auth_check:
-        raise AuthenticationException()
+        auth_check = (hashed_password == encrypted_password)
+        print ("Success" if auth_check else "Failure")
+
+        if not auth_check:
+            raise AuthenticationException()
+    except AuthenticationException:
+        raise
+    except Exception:
+        print ("ERROR")
+        raise
