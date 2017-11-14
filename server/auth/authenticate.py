3eaf1d352c377f34db9959ec41b994165f6ae5cd
carrotpaul <developer.rabbitt@gmail.com>
2017-11-13 21:48:42 -0500

    Updating Authenticator to work with new changes to api_certificate format

--- server/auth/authenticate.py
+++ server/auth/authenticate.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 from binascii import unhexlify
-import os, ast, hashlib
+import os, ast, re, hashlib
 
 class AuthenticationException(Exception):
     pass
 
 def get_auth_cert():
     with open('/run/secrets/api_auth_certificate', 'r') as cert:
-        auth_params_dict = ast.literal_eval(cert.read().replace('\n ', ''))
+        auth_params_dict = ast.literal_eval(re.sub(r'\n[^\S\n]+', '', cert.read()))
         return auth_params_dict['encrypted_password'], auth_params_dict['pepper']
 
 def salt_and_pepper(byte_password, pepper):
@@ -18,19 +18,13 @@ def salt_and_pepper(byte_password, pepper):
 def authenticate(password):
     print ("Authenticating event....", end='')
 
-    try:
-        encrypted_password, pepper = get_auth_cert()
-        encrypted_password = unhexlify(encrypted_password)
-        hashed_password = salt_and_pepper(bytearray(password, 'utf-8'),
-            bytearray(pepper.decode('string-escape')))
+    encrypted_password, pepper = get_auth_cert()
+    encrypted_password = unhexlify(encrypted_password)
+    hashed_password = salt_and_pepper(bytearray(password, 'utf-8'),
+        bytearray(pepper.decode('string-escape')))
 
-        auth_check = (hashed_password == encrypted_password)
-        print ("Success" if auth_check else "Failure")
+    auth_check = (hashed_password == encrypted_password)
+    print ("Success" if auth_check else "Failure")
 
-        if not auth_check:
-            raise AuthenticationException()
-    except AuthenticationException:
-        raise
-    except Exception:
-        print ("ERROR")
-        raise
+    if not auth_check:
+        raise AuthenticationException()
