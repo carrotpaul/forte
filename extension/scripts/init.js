3b9f31357c73582b036d4188ec4426d9283d61fd
carrotpaul <developer.rabbitt@gmail.com>
2018-05-26 23:44:15 -0400

    Only activate for supported sites

--- extension/scripts/init.js
+++ extension/scripts/init.js
@@ -1,14 +1,28 @@
+function buildRules() {
+  /* Youtube-DL supports A LOT of sites; I really only want a very small subset.
+   * Scraping the supportedSites.md page seems more effort, so I'll just maintain
+   * my own list in here.
+   */
+  var supportedSites = [
+    'www.youtube.com',
+    'www.soundcloud.com',
+    'www.nicovideo.jp/'
+  ];
+
+  return supportedSites.map(function(site) {
+    return {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: site }
+        })
+      ],
+      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    }
+  });
+}
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'chrome://' },
-          })
-        ],
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
+    chrome.declarativeContent.onPageChanged.addRules(buildRules());
   });
 });
