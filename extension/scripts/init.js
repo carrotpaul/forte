ffbaa1bb7651c12730406eced36caf530954f3ae
carrotpaul <developer.rabbitt@gmail.com>
2018-05-28 14:20:56 -0400

    Push download request to Kafka

--- extension/scripts/init.js
+++ extension/scripts/init.js
@@ -1,28 +1,11 @@
-function buildRules() {
-  /* Youtube-DL supports A LOT of sites; I really only want a very small subset.
-   * Scraping the supportedSites.md page seems more effort, so I'll just maintain
-   * my own list in here.
-   */
-  var supportedSites = [
-    'www.youtube.com',
-    'www.soundcloud.com',
-    'www.nicovideo.jp/'
-  ];
-
-  return supportedSites.map(function(site) {
-    return {
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { hostEquals: site }
-        })
-      ],
-      actions: [ new chrome.declarativeContent.ShowPageAction() ]
-    }
-  });
-}
-
+// Add rules to enable the extension for supported sites only
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules(buildRules());
+    chrome.declarativeContent.onPageChanged.addRules(buildActivationRules());
   });
+});
+
+// Listen to when someone clicks on the extension
+chrome.pageAction.onClicked.addListener(function(tab) {
+  sendDownloadRequest(tab);
 });
